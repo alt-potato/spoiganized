e2905243573d10d90bc92f5ccf9cc2a6f2fd635e
alt-potato <jason.why.zhuang@gmail.com>
2026-02-27 22:54:24 -0500

    Modify mush and add related advancement

--- src/main/java/com/meconeko/spoiganized/ModCreativeTabs.java
+++ src/main/java/com/meconeko/spoiganized/ModCreativeTabs.java
@@ -1,7 +1,9 @@
 package com.meconeko.spoiganized;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,6 +14,8 @@ public class ModCreativeTabs {
 
     public static final RegistryObject<CreativeModeTab> BASE = CREATIVE_MODE_TABS.register("base",
             () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.spoiganized.base"))
+                    .withTabsBefore(CreativeModeTabs.FOOD_AND_DRINKS)
                     .icon(() -> ModItems.MUSH.get().getDefaultInstance())
                     .displayItems((parameters, output) -> {
                         output.accept(ModItems.MUSH.get());
--- src/main/java/com/meconeko/spoiganized/ModItems.java
+++ src/main/java/com/meconeko/spoiganized/ModItems.java
@@ -1,5 +1,7 @@
 package com.meconeko.spoiganized;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,7 +16,12 @@ public class ModItems {
     @SuppressWarnings("null")
     public static final RegistryObject<Item> MUSH = ITEMS.register("mush",
             () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-                    .alwaysEat().nutrition(1).saturationMod(2f).build())));
+                    .alwaysEat()
+                    .nutrition(1)
+                    .saturationMod(0.4f)
+                    .fast()
+                    .effect(() -> new MobEffectInstance(MobEffects.HUNGER, 30 * 20), 0.8F)
+                    .build())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
